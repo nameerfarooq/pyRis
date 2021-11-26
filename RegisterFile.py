ced1a64767234ae4251127622059c25b3696d346
nameerfarooq <nameerfarooquit@gmail.com>
2021-11-27 00:04:03 +0500

    3rd

--- RegisterFile.py
+++ RegisterFile.py
@@ -1,5 +1,5 @@
-from typing import List
 from myhdl import * 
+import array as arr
 rows = 32
 DW = 2**(rows-1)
 RegArray = [Signal(intbv(0,-DW,DW)) for i in range(rows+1)]
@@ -13,31 +13,31 @@ def RegFile(rs_A,rs_B,Rd,WriteBack , WriteEnable, Data_A, Data_B):
     @always_comb
     def write():
         if WriteEnable == 1 and int(Rd) != 0 :
-            RegArray[int(Rd)].next =  WriteBack
+            RegArray[Rd].next =  WriteBack
     
     return read, write
 @block
 def SimulateReg():
-    rs_A = Signal(int(32))
-    rs_B = Signal(int(13))
-    Rd = Signal(int(32))
-    WriteBack = Signal(int(200))
-    writeEnable = Signal(int(1))
-    Data_A = Signal(int(0))
-    Data_B = Signal(int(0))
+    rs_A = Signal(intbv(32,0,rows+1))
+    rs_B = Signal(intbv(13,0,rows+1))
+    Rd = Signal(intbv(32,0,rows+1))
+    WriteBack = Signal(intbv(200,-DW,DW))
+    writeEnable = Signal(intbv(1,0,2))
+    Data_A = Signal(intbv(0,-DW,DW))
+    Data_B = Signal(intbv(0,-DW,DW))
     regg = RegFile(rs_A,rs_B,Rd,WriteBack,writeEnable,Data_A,Data_B)
     regg.convert('Verilog')
     @instance
     def simulatingReg():
         for i in range(2):
             yield delay(10)
-            print("rs_a : ",rs_A)
-            print("rs_b : ",rs_B)
-            print("rd : ",Rd)
-            print("writeback : ",WriteBack)
-            print("writeEnable : ",writeEnable)
-            print("data A : ",Data_A)
-            print("data B : ",Data_B)
+            print("rs_a : ",int(rs_A))
+            print("rs_b : ",int(rs_B))
+            print("rd : ",int(Rd))
+            print("writeback : ",int(WriteBack))
+            print("writeEnable : ",int(writeEnable))
+            print("data A : ",int(Data_A))
+            print("data B : ",int(Data_B))
             print("-------------------------------------")
     return regg, simulatingReg
 
