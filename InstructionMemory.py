4211bf298fad28682c1a404e39d88cd85ebfaec0
nameerfarooq <nameerfarooquit@gmail.com>
2021-11-29 15:50:06 +0500

    instructionMemory added

---
+++ InstructionMemory.py
@@ -0,0 +1,40 @@
+from myhdl import *
+DW = 32
+AW = 5
+ram = 1000
+@block
+def InstructionMemory(adr,intructionOut,instructions):
+    @always_comb
+    def read():
+        intructionOut.next = instructions[int(adr)]
+    return read
+
+@block
+def SimulateIM():
+    instructions = [0x00500193,
+0x00500113,
+0x00500213,
+0xfa610193,
+0x00510193,
+0x00418663,
+0x00000013,
+0x00302423]
+    adr = Signal(intbv(0,0,len(instructions)))
+    instructionOut = Signal(intbv(0,0,2**DW,DW))
+
+    IM = InstructionMemory(adr, instructionOut, instructions )
+    # IM.convert('Verilog')
+    adrList = [0,1,2,3,4,5,6,7]
+    @instance
+    def simulatorIM():
+        for i in adrList:
+            adr.next = i
+            yield delay(10)
+            print("address", int(adr))
+            print("instruction out",instructionOut)
+
+    return simulatorIM, IM
+
+tb = SimulateIM()
+tb.config_sim(trace=True)
+tb.run_sim()
--- RegisterFile.py
+++ RegisterFile.py
@@ -18,18 +18,18 @@ def RegFile(rs_A,rs_B,Rd,WriteBack , WriteEnable, Data_A, Data_B):
     return read, write
 @block
 def SimulateReg():
-    rs_A = Signal(intbv(32,0,rows+1))
+    rs_A = Signal(intbv(1,0,rows+1))
     rs_B = Signal(intbv(13,0,rows+1))
-    Rd = Signal(intbv(32,0,rows+1))
+    Rd = Signal(intbv(1,0,rows+1))
     WriteBack = Signal(intbv(200,-DW,DW))
     writeEnable = Signal(intbv(1,0,2))
     Data_A = Signal(intbv(0,-DW,DW))
     Data_B = Signal(intbv(0,-DW,DW))
     regg = RegFile(rs_A,rs_B,Rd,WriteBack,writeEnable,Data_A,Data_B)
-    regg.convert('Verilog')
+    # regg.convert('Verilog')
     @instance
     def simulatingReg():
-        for i in range(2):
+        for i in range(1):
             yield delay(10)
             print("rs_a : ",int(rs_A))
             print("rs_b : ",int(rs_B))
