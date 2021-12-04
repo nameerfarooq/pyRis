530612e8c6410ca57f9f189f36060982bdd98e3f
nameerfarooq <nameerfarooquit@gmail.com>
2021-12-04 23:16:19 +0500

    git commit -m

--- PC.py
+++ PC.py
@@ -7,7 +7,29 @@ def PC(clk,input,pc,pc4):
     def read():
         pc.next = reg 
         pc4.next = reg + 4
-    always(clk.posedge)
+    @always_comb
     def write():
-        reg.next = input
-    return read, write
+        if clk == 1:
+            reg.next = input
+    return write, read
+
+@block
+def SimulatePC():
+    clk = Signal(bool(False))
+    input = Signal(intbv(10,0,2**32,32))
+    pc = Signal(intbv(0,0,2**32,32))
+    pc4 = Signal(intbv(0,0,2**32,32))
+    runPC = PC(clk,input,pc,pc4)
+    runPC.convert('Verilog')
+    @instance
+    def Run():
+        yield delay(10)
+        print("clk : ",clk)
+        print("input : ",input)
+        print("pc : ",pc)
+        print("pc4 : ",pc4)
+    return Run, runPC
+
+a = SimulatePC()
+a.config_sim(trace=True)
+a.run_sim()
