c9e9f80f5796b19b0e75fde4798f51f1b8c148b4
nameerfarooq <nameerfarooquit@gmail.com>
2021-12-05 23:32:46 +0500

    core 1st commit

---
+++ Core.py
@@ -0,0 +1,10 @@
+from myhdl import *
+
+from ALU import ALU
+from ALUControl import ALUcontrol
+from ControlUnit import ControlUnit
+from DataMemory import DataMemory
+from InstructionMemory import InstructionMemory
+from RegisterFile import RegFile
+from ImmediateGenerator import ImmediateGen
+from PC import PC
