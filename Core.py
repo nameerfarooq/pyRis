e9b7eed75c24ab0b9ef8a9c49a049341b683ea05
nameerfarooq <nameerfarooquit@gmail.com>
2021-12-06 02:04:39 +0500

    2nd for core

--- Core.py
+++ Core.py
@@ -8,3 +8,64 @@ from InstructionMemory import InstructionMemory
 from RegisterFile import RegFile
 from ImmediateGenerator import ImmediateGen
 from PC import PC
+
+@block
+def Core(clk,Reset):
+    @always_seq(clk.posedge)
+    def TopModule():
+        
+        # PC
+        
+        pc_out,pc4_out,pc_in = [Signal(intbv(0,0,2**32,32))]
+        pc = PC(clk,pc_in,pc_out,pc4_out)
+        
+        # INSTRUCTION MEMORY
+
+        instructionsList = [0x00500193,0x00500113,0x00500213,0xfa610193,0x00510193,0x00418663,0x00000013,0x00302423]
+        instructionOut = Signal(intbv(0,0,2**32,32))
+        inst_mem = InstructionMemory(pc,instructionOut,instructionsList)
+
+        # Control Unit
+        
+        CUsignals = [Signal(intbv(0)[1:]) for i in range(9)]
+        branchOut, RegWriteOut, ImmediateOut, LoadOut, StoreOut, UtypeOut, AuipcOut, JalOut, JalrOut = [Signal(intbv(0)[1:]) for i in range(9)]
+        CU = ControlUnit(instructionOut[6:],*CUsignals,branchOut, RegWriteOut, ImmediateOut, LoadOut, StoreOut, UtypeOut, AuipcOut, JalOut, JalrOut)
+
+        # Immediate Generator
+        
+        s_imm, sb_imm, uj_imm, u_imm, i_imm = [Signal(intbv(0)[32:]) for i in range(5)]
+        ImmediateGenerator = ImmediateGen(instructionOut,pc_out,s_imm, sb_imm, uj_imm, u_imm, i_imm)
+
+        # Register File
+        bus_A, bus_B = Signal(intbv(0)[32:])
+        
+        #   ///// write back logic
+
+        writeBack = Signal(0)[32:]
+        if JalOut == 1:
+            writeBack.next = pc4_out
+        elif UtypeOut == 1:
+            writeBack.next == u_imm
+        elif LoadOut == 1:
+            writeBack.next == Data_out
+        else:
+            writeBack.next = result
+
+        #  ///// rd logic
+
+        rd = Signal(0)[5:]
+        if JalOut == 1 :
+            rd.next = 1
+        else:
+            rd.next = instructionOut[11,7]
+                                         # rs_a        rs_b                    rd writeback , writeEnable , dataA, dataB               
+        regiterFile = RegFile(clk,instructionOut[19:15], instructionOut[24:20],rd,writeBack, RegWriteOut , bus_A , bus_B)
+
+        #  ALU CONTROL
+
+        aluControlpin = Signal(intbv(0,0,(2**32)-1))
+        aluControl = ALUcontrol(instructionOut[14:12], instructionOut[30],branchOut,aluControlpin)
+
+        # ALU
+
+        
--- RegisterFile.py
+++ RegisterFile.py
@@ -1,10 +1,11 @@
 from myhdl import * 
+import myhdl
 import array as arr
-rows = 32
-DW = 2**(rows-1)
-RegArray = [Signal(intbv(0,-DW,DW)) for i in range(rows+1)]
 @block
-def RegFile(rs_A,rs_B,Rd,WriteBack , WriteEnable, Data_A, Data_B):
+def RegFile(clk,rs_A,rs_B,Rd,WriteBack , WriteEnable, Data_A, Data_B):
+    rows = 32
+    DW = 2**(rows-1)
+    RegArray = [Signal(intbv(0,-DW,DW)) for i in range(rows+1)]
 
     @always_comb
     def read():
@@ -12,36 +13,38 @@ def RegFile(rs_A,rs_B,Rd,WriteBack , WriteEnable, Data_A, Data_B):
         Data_B.next = RegArray[int(rs_B)]
     @always_comb
     def write():
-        if WriteEnable == 1 and int(Rd) != 0 :
-            RegArray[Rd].next =  WriteBack
+        if clk ==1:
+            if WriteEnable == 1 and int(Rd) != 0 :
+                RegArray[Rd].next =  WriteBack
     
     return read, write
-@block
-def SimulateReg():
-    rs_A = Signal(intbv(1,0,rows+1))
-    rs_B = Signal(intbv(13,0,rows+1))
-    Rd = Signal(intbv(1,0,rows+1))
-    WriteBack = Signal(intbv(200,-DW,DW))
-    writeEnable = Signal(intbv(1,0,2))
-    Data_A = Signal(intbv(0,-DW,DW))
-    Data_B = Signal(intbv(0,-DW,DW))
-    regg = RegFile(rs_A,rs_B,Rd,WriteBack,writeEnable,Data_A,Data_B)
-    # regg.convert('Verilog')
-    @instance
-    def simulatingReg():
-        for i in range(1):
-            yield delay(10)
-            print("rs_a : ",int(rs_A))
-            print("rs_b : ",int(rs_B))
-            print("rd : ",int(Rd))
-            print("writeback : ",int(WriteBack))
-            print("writeEnable : ",int(writeEnable))
-            print("data A : ",int(Data_A))
-            print("data B : ",int(Data_B))
-            print("-------------------------------------")
-    return regg, simulatingReg
+# @block
+# def SimulateReg():
+#     rs_A = Signal(intbv(1,0,rows+1))
+#     rs_B = Signal(intbv(13,0,rows+1))
+#     Rd = Signal(intbv(1,0,rows+1))
+#     WriteBack = Signal(intbv(200,-DW,DW))
+#     writeEnable = Signal(intbv(1,0,2))
+#     Data_A = Signal(intbv(0,-DW,DW))
+#     Data_B = Signal(intbv(0,-DW,DW))
+#     regg = RegFile(rs_A,rs_B,Rd,WriteBack,writeEnable,Data_A,Data_B)
+#     regg.convert('Verilog')
+#     @instance
+#     def simulatingReg():
+#         for i in range(1):
+#             yield delay(10)
+#             print("rs_a : ",int(rs_A))
+#             print("rs_b : ",int(rs_B))
+#             print("rd : ",int(Rd))
+#             print("writeback : ",int(WriteBack))
+#             print("writeEnable : ",int(writeEnable))
+#             print("data A : ",int(Data_A))
+#             print("data B : ",int(Data_B))
+#             print("-------------------------------------")
+#             print('\nUsing MyHDL version: {}'.format(myhdl.__version__))
+#     return regg, simulatingReg
 
 
-tb = SimulateReg()
-tb.config_sim(trace=True)
-tb.run_sim()
+# tb = SimulateReg()
+# tb.config_sim(trace=True)
+# tb.run_sim()
